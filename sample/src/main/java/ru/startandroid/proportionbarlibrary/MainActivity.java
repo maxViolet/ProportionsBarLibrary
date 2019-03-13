3cbba74b4da80e69b1f5a5954fbbb4c09a4ec233
maxViolet <maxfialko@gmail.com>
2019-03-13 23:14:39 +0300

    refactor (examples now work properly)

--- sample/src/main/java/ru/startandroid/proportionbarlibrary/MainActivity.java
+++ sample/src/main/java/ru/startandroid/proportionbarlibrary/MainActivity.java
@@ -19,14 +19,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setup1stExample();
-//        setup2ndExample();
-//        setup3rdExample();
+        setup2ndExample();
+        setup3rdExample();
         setup4thExample();
     }
 
     private void setup1stExample() {
         proportionsBar1 = new ProportionsBar(this);
-        proportionsBar1.setId(R.id.proportionsBar1);
+        proportionsBar1.setId(R.id.proportion_bar_1);
         proportionsBar1.showGaps(true)
                 .gapSize(1.2)
                 .showRoundEdges(true)
@@ -37,13 +37,13 @@ public class MainActivity extends AppCompatActivity {
                 .addIntColor(getResources().getColor(R.color.example1_3))
                 .addValues(33, 22, 28);
 
-        LinearLayout container1 = findViewById(R.id.proportionsBar1);
+        LinearLayout container1 = findViewById(R.id.container_pb1);
         container1.addView(proportionsBar1);
     }
 
     private void setup2ndExample() {
         proportionsBar2 = new ProportionsBar(this);
-        proportionsBar1.setId(R.id.proportionsBar2);
+        proportionsBar2.setId(R.id.proportion_bar_2);
         proportionsBar2.showRoundEdges(false)
                 .showGaps(false)
                 .addIntColors(getResources().getColor(R.color.example2_1),
@@ -51,13 +51,13 @@ public class MainActivity extends AppCompatActivity {
                         getResources().getColor(R.color.example2_3))
                 .addValues(11, 30, 24, 12, 41, 27);
 
-        LinearLayout container2 = findViewById(R.id.proportionsBar2);
+        LinearLayout container2 = findViewById(R.id.container_pb2);
         container2.addView(proportionsBar2);
     }
 
     private void setup3rdExample() {
         proportionsBar3 = new ProportionsBar(this);
-        proportionsBar1.setId(R.id.proportionsBar3);
+        proportionsBar3.setId(R.id.proportion_bar_3);
         proportionsBar3.showGaps(true)
                 .showRoundEdges(false)
                 .gapSize(0.3)
@@ -68,13 +68,13 @@ public class MainActivity extends AppCompatActivity {
                         getResources().getColor(R.color.example3_5))
                 .addValues(33, 2, 11, 40, 20);
 
-        LinearLayout container3 = findViewById(R.id.proportionsBar3);
+        LinearLayout container3 = findViewById(R.id.container_pb3);
         container3.addView(proportionsBar3);
     }
 
     private void setup4thExample() {
         proportionsBar4 = new ProportionsBar(this);
-        proportionsBar1.setId(R.id.proportionsBar4);
+        proportionsBar4.setId(R.id.proportion_bar_4);
         proportionsBar4.showGaps(true)
                 .gapSize(0.7)
                 .animated(true)
@@ -86,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
                         getResources().getColor(R.color.example4_3))
                 .addValues(4276, 3238, 2820);
 
-        LinearLayout container4 = findViewById(R.id.proportionsBar4);
+        LinearLayout container4 = findViewById(R.id.container_pb4);
         container4.addView(proportionsBar4);
     }
 }
