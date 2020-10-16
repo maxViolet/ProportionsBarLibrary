f2c44486fb62630bfc80e2ba045eab033b31c070
maxviolet <m.fialko@indriver.com>
2020-10-16 21:23:39 +0300

    deleted tests, refreshed some dependencies, started new kotlin main view class

---
+++ proportionsbarlibrary/src/main/java/com/github/proportionsbarlibrary/ProportionsBarKotlin.kt
@@ -0,0 +1,14 @@
+package com.github.proportionsbarlibrary
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class ProportionsBarKotlin @JvmOverloads constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+
+
+}
